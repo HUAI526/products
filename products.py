16664de0ff7feaf6c1bdea2ef0b5e9effd04557f
Huai <leonowpower@gmail.com>
2020-11-23 20:03:59 +0800

    add check filee

--- products.py
+++ products.py
@@ -1,6 +1,13 @@
+#載入作業系統
+import os
+
 #讀取檔案
+
 products= []
-with open('products.csv', 'r', encoding='utf-8') as f:
+if os.path.isfile('products.csv'):
+	#從os模組裡面選用isfile模組檢查檔案，不用硬背要使用再去搜尋功能就好
+	print('yes')
+	with open('products.csv', 'r', encoding='utf-8') as f:
 	#讀取檔案也要用寫出的編碼utf8
 	for line in f:
 		if '商品,價格' in line:
@@ -10,8 +17,12 @@ with open('products.csv', 'r', encoding='utf-8') as f:
 		#name跟price要去除換行符號然後使用split切割
 		#遇到逗號就切割，分成name, price
 		products.append([name, price])
+else:
+	print('no')
 
 
+
+#讓使用者輸入購買產品和金額
 while True:
 	name = input('請輸入商品名稱：')
 	if name == 'q':
@@ -21,9 +32,11 @@ while True:
 	products.append([name, price])#在清單中加入小清單有名字跟價格
 print(products)
 
+#印出所有購買紀錄
 for p in products:
 	print(p[0], '的價格是', p[1])
 
+#寫入檔案
 with open('products.csv', 'w' , encoding='utf-8') as f:#寫入檔案存成csv檔並用編碼utf8寫
 	f.write('商品,價格\n')
 	for p in products:
